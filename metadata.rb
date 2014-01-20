20660d8e590b679dd8f8cdb3160861d7b2e96bc5
Phil Cohen <github@phlippers.net>
2014-01-19 22:28:25 -0800

    update metadata with new recipe

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version          "1.0.0"
 
 recipe "mdadm", "Install mdadm"
+recipe "mdadm::degraded-raid-fix", "Recipe for fixing a boot race condition where Ubuntu thinks the RAID is degraded."
 
-%w[debian ubuntu].each do |os|
-  supports os
-end
+supports "debian"
+supports "ubuntu"
