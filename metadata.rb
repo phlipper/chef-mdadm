834cb1f75f9e53e712449e5e3d67acb699112ccb
Phil Cohen <github@phlippers.net>
2014-01-19 22:44:47 -0800

    bump version to 1.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "github@phlippers.net"
 license          "MIT"
 description      "Installs mdadm"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "1.0.0"
+version          "1.0.1"
 
 recipe "mdadm", "Install mdadm"
 recipe "mdadm::degraded-raid-fix", "Recipe for fixing a boot race condition where Ubuntu thinks the RAID is degraded."
